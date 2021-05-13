648a7e01b0650994675caedbbe22c98bbd685fed
146-creator <80502971+146-creator@users.noreply.github.com>
2021-05-13 15:26:00 +0800

    Update python.py

--- python.py
+++ python.py
@@ -1,3 +1,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+data = np.array[[1, 2, 3],[2, 3, 4]]
+df = pd.dataframe(data,index = [1,2])
+fig, ax = plt.subplot()
+ax.plot(df)
+ax.set_title('practice')
+plt.show()
